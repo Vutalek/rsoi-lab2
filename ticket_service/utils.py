0a7d2cf7946ad1827865990c28da3edd43e35153
vuta <mr.sergei02@mail.ru>
2026-09-26 17:43:21 +0300

    Добавил в compose запуск ticket_service
    ещё дополнительно исправил баг: ticket_service читал не те переменные среды

--- ticket_service/utils.py
+++ ticket_service/utils.py
@@ -3,12 +3,12 @@ import os
 from sqlalchemy import create_engine
 
 def construct_engine():
-    protocol = os.environ.get("FLIGHT_SERVICE_DB_PROTOCOL", "")
-    user = os.environ.get("FLIGHT_SERVICE_DB_USER", "")
-    password = os.environ.get("FLIGHT_SERVICE_DB_PASSWORD", "")
-    host = os.environ.get("FLIGHT_SERVICE_DB_HOST", "")
-    port = os.environ.get("FLIGHT_SERVICE_DB_PORT", "")
-    name = os.environ.get("FLIGHT_SERVICE_DB_NAME", "")
+    protocol = os.environ.get("TICKET_SERVICE_DB_PROTOCOL", "")
+    user = os.environ.get("TICKET_SERVICE_DB_USER", "")
+    password = os.environ.get("TICKET_SERVICE_DB_PASSWORD", "")
+    host = os.environ.get("TICKET_SERVICE_DB_HOST", "")
+    port = os.environ.get("TICKET_SERVICE_DB_PORT", "")
+    name = os.environ.get("TICKET_SERVICE_DB_NAME", "")
 
     return create_engine(
         f"{protocol}://{user}:{password}@{host}:{port}/{name}"
